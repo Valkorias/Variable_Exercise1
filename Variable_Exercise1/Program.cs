8ad4d9edc23c180565dc71227397ffa04f46af03
David Figone <david.louis.figone@gmail.com>
2022-10-13 11:29:55 -0500

    Add project files.

---
+++ Variable_Exercise1/Program.cs
@@ -0,0 +1,14 @@
+﻿// Declare and Initialize variables
+string name = "Jack";
+int age = 21;
+char middleInitial = 'C';
+bool isOver18 = true;
+double currentTemp = 70.8;
+decimal currentPrice = 25.99m;
+
+Console.WriteLine($"Name: {name}");
+Console.WriteLine($"Age: {age}");
+Console.WriteLine($"Middle Initial: {middleInitial}");
+Console.WriteLine($"Is over 18: {isOver18}");
+Console.WriteLine($"Current Tempature: {currentTemp}");
+Console.WriteLine($"Current Price: {currentPrice}");
